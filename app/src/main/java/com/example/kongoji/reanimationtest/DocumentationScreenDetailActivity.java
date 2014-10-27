4c9187e5f8df3bec4de985e286f34164562db0cb
danielreinhardt <gaaaaraa@gmail.com>
2014-10-27 17:29:33 +0100

    prepared library for fragment navigation

--- app/src/main/java/com/example/kongoji/reanimationtest/DocumentationScreenDetailActivity.java
+++ app/src/main/java/com/example/kongoji/reanimationtest/DocumentationScreenDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.kongoji.reanimationtest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
